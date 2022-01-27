ff507770d47dbb925da89d3a456c304817a3a977
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-01-27 17:26:20 +0100

    Completed 3/6 - User menu

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        new SearchEngine().run();
+        new UserInterface().start();
     }
 }
 
@@ -17,13 +17,7 @@ class SearchEngine {
     public final String SEPARATOR = " ";
     private final Repository repository = new Repository();
 
-    public void run() {
-        initRepository();
-        searchingData();
-
-    }
-
-    private void initRepository() {
+    public void initRepository() {
         int number = Util.getNumberOfPeople();
         System.out.println("Enter all people:");
         List<DataModel> storage = repository.getStorage();
@@ -36,32 +30,34 @@ class SearchEngine {
         System.out.println();
     }
 
-    private void searchingData() {
-        int queryNumber = Util.getQueriesNumber();
+    public void searchingData() {
 
         List<DataModel> storage = repository.getStorage();
 
-        for (int j = 0; j < queryNumber; j++) {
-            String queryWord = Util.getQueryWord();
-            List<Integer> resultIndex = new ArrayList<>();
-            for (int i = 0; i < storage.size(); i++) {
-                DataModel dataModel = storage.get(i);
-                if (processSearching(dataModel, queryWord)) {
-                    resultIndex.add(i);
-                }
+        String queryWord = Util.getQueryWord();
+        List<Integer> resultIndex = new ArrayList<>();
+        for (int i = 0; i < storage.size(); i++) {
+            DataModel dataModel = storage.get(i);
+            if (processSearching(dataModel, queryWord)) {
+                resultIndex.add(i);
             }
-            Util.printResult(storage, resultIndex);
         }
+        Util.printResult(storage, resultIndex);
     }
 
     private boolean processSearching(DataModel dataModel, String queryWord) {
         String[] content = dataModel.getDataLine().toLowerCase(Locale.ROOT).split(SEPARATOR);
         for (String each : content) {
-            if(each.contains(queryWord.toLowerCase(Locale.ROOT))){
+            if (each.contains(queryWord.toLowerCase(Locale.ROOT))) {
                 return true;
             }
         }
         return false;
+    }
+
+    public void printAllData() {
+        List<DataModel> storage = repository.getStorage();
+        Util.printResult(storage);
     }
 }
 
@@ -69,15 +65,8 @@ class Util {
 
     static Scanner scanner = new Scanner(System.in);
 
-    public static int getQueriesNumber() {
-        System.out.println("Enter the number of search people:");
-        int result = Integer.parseInt(scanner.nextLine());
-        System.out.println();
-        return result;
-    }
-
     public static String getQueryWord() {
-        System.out.println("Enter data to search people:");
+        System.out.println("\nEnter a name or email to search all suitable people.");
         String result = scanner.nextLine();
         System.out.println();
         return result;
@@ -85,14 +74,20 @@ class Util {
 
     public static void printResult(List<DataModel> storage, List<Integer> resultIndex) {
         if (resultIndex.size() > 0) {
-            System.out.println("Found people:");
-            for(Integer each: resultIndex) {
+            for (Integer each : resultIndex) {
                 System.out.println(storage.get(each).getDataLine());
             }
         } else {
             System.out.println("No matching data found.");
         }
         System.out.println();
+    }
+
+    public static void printResult(List<DataModel> storage) {
+        System.out.println("\n=== List of people ===");
+        for (DataModel each : storage) {
+            System.out.println(each.getDataLine());
+        }
     }
 
     public static int getNumberOfPeople() {
@@ -131,5 +126,45 @@ class DataModel {
 
     public void setDataLine(String dataLine) {
         this.dataLine = dataLine;
+    }
+}
+
+class UserInterface {
+
+    public Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+        SearchEngine searchEngine = new SearchEngine();
+        searchEngine.initRepository();
+        int option;
+        do {
+            option = choiceMainMenu();
+            switch (option) {
+                case 1:
+                    searchEngine.searchingData();
+                    break;
+                case 2:
+                    searchEngine.printAllData();
+                    break;
+                case 0:
+                    System.out.println("Bye!");
+                    break;
+            }
+        } while (option != 0);
+    }
+
+    public int choiceMainMenu() {
+        int option;
+        do {
+            System.out.println("=== Menu ===\n" +
+                    "1. Find a person\n" +
+                    "2. Print all people\n" +
+                    "0. Exit");
+            option = Integer.parseInt(scanner.nextLine());
+            if (option < 0 || option > 2) {
+                System.out.println("Incorrect option! Try again.\n");
+            }
+        } while (option < 0 || option > 2);
+        return option;
     }
 }

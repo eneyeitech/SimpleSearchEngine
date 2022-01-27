0cb54d6fffe665beff7c7b100d853201afb111ad
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-01-27 09:58:32 +0100

    Completed 1/6 - String theory

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,7 +1,23 @@
 package search;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String line1 = scanner.nextLine();
+        String line2 = scanner.nextLine();
+        List<String> list = List.of(line1.split(" "));
+        List<String> search = List.of(line2);
+
+        int index = Collections.indexOfSubList(list, search);
+
+        if (index == -1){
+            System.out.println("Not found");
+        } else {
+            System.out.println(index + 1);
+        }
     }
 }
